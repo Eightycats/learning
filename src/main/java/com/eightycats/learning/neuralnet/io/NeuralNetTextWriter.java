b9fb04742ad3bf8f1f61df0289682bd3f002f3fa
Matt Jensen <eightycats@gmail.com>
2016-05-10 11:43:27 -0700

    Adding license headers.

--- src/main/java/com/eightycats/learning/neuralnet/io/NeuralNetTextWriter.java
+++ src/main/java/com/eightycats/learning/neuralnet/io/NeuralNetTextWriter.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2016 Matthew A Jensen <eightycats@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.eightycats.learning.neuralnet.io;
 
 import java.io.PrintStream;
@@ -8,69 +22,53 @@ import com.eightycats.learning.neuralnet.Neuron;
 import com.eightycats.learning.neuralnet.NeuralNet;
 
 /**
- *
- *
- *
+ * Logs the values of a neural net as plain text.
  */
 public class NeuralNetTextWriter implements NeuralNetWriter
 {
+    protected static NeuralNetTextWriter writer = new NeuralNetTextWriter();
 
-    private static NeuralNetTextWriter writer = new NeuralNetTextWriter();
-
-    public static void print( NeuralNet network )
+    public static void print (NeuralNet network)
     {
-       writeNetwork( network, System.out );
+        writeNetwork(network, System.out);
     }
 
-    public static void writeNetwork( NeuralNet network, PrintStream output )
+    public static void writeNetwork (NeuralNet network, PrintStream output)
     {
-        writer.write( network, output );
+        writer.write(network, output);
     }
 
-    public void write( NeuralNet network, PrintStream output )
+    @Override
+    public void write (NeuralNet network, PrintStream output)
     {
-        output.println( "Network:" );
+        output.println("Network:");
 
-        output.println( "Learning rate       : " +
-                        network.getLearningRate() );
-        output.println( "Learning rate decay : " +
-                        network.getLearningRateDecay() );
-        output.println( "Momentum            : " +
-                        network.getMomentum() );
+        output.println("Learning rate       : " + network.getLearningRate());
+        output.println("Learning rate decay : " + network.getLearningRateDecay());
+        output.println("Momentum            : " + network.getMomentum());
 
         InputLayer inputs = network.getInputLayer();
-        output.println( "Input count         : " +
-                        inputs.getInputCount() );
-        output.println( "Input bias          : " +
-                        inputs.getBias() );
+        output.println("Input count         : " + inputs.getInputCount());
+        output.println("Input bias          : " + inputs.getBias());
 
         int layerCount = network.getLayerCount();
 
-        for( int i = 0; i < layerCount; i++ )
-        {
-            output.println( "Layer ["+i+"]:" );
+        for (int i = 0; i < layerCount; i++) {
+            output.println("Layer [" + i + "]:");
             Layer layer = network.getLayer(i);
 
             int neuronCount = layer.getNeuronCount();
-            for( int j = 0; j < neuronCount; j++ )
-            {
-
-                output.println( "Neuron ["+j+"]:" );
+            for (int j = 0; j < neuronCount; j++) {
+                output.println("Neuron [" + j + "]:");
                 Neuron neuron = layer.getNeuron(j);
                 int weightCount = neuron.getWeightCount();
-                for( int k = 0; k < weightCount; k++ )
-                {
-                    output.println( "Weight ["+k+"]: " + neuron.getWeight(k) );
+                for (int k = 0; k < weightCount; k++) {
+                    output.println("Weight [" + k + "]: " + neuron.getWeight(k));
                 }
-
                 output.println();
-
             }
-
         }
 
         output.flush();
-
     }
-
 }
--- src/main/java/com/eightycats/learning/neuralnet/train/TrainingHarness.java
+++ src/main/java/com/eightycats/learning/neuralnet/train/TrainingHarness.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2016 Matthew A Jensen <eightycats@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.eightycats.learning.neuralnet.train;
 
 import com.eightycats.learning.neuralnet.InputSource;
@@ -8,8 +22,7 @@ import com.eightycats.math.functions.Processor;
 import com.eightycats.math.util.VectorMath;
 
 /**
- *
- *
+ * A harness for training a network.
  */
 public class TrainingHarness
 {
@@ -34,7 +47,6 @@ public class TrainingHarness
 
     public void train (NeuralNet network, InputSource inputSource, int trainingRounds)
     {
-
         IncrementalAverage averageError = new IncrementalAverage();
 
         for (int count = 0; count < trainingRounds; count++) {
@@ -60,7 +72,6 @@ public class TrainingHarness
 
             averageError.add(error);
             Logger.debug("Average error: " + averageError.getAverage());
-
         }
 
         Logger.debug(network.toString());
